36c6a5ab2c891063b5f889d278c5cc383cf20f5c
Andrew <drew982@yahoo.com>
2016-12-29 21:30:30 -0600

    Finish Spock Testing

--- src/test/groovy/CalculatorTest.groovy
+++ src/test/groovy/CalculatorTest.groovy
@@ -1,5 +1,4 @@
 import spock.lang.Specification
-
 /**
  * Created by Andrew on 12/29/2016.
  */
@@ -8,5 +7,19 @@ class CalculatorTest extends Specification
     void setup()
     {
 
+    }
+
+    def "Calculator's add method calls andrewMath.add"() //Interaction testing using mocks ("half-classes")
+    {
+        given:
+        AndrewMath math = Mock(AndrewMath)
+        Calculator calc = new Calculator()
+        calc.andrewMath = math
+
+        when:
+        7.times {calc.add(1, 1)} //x.times { } needed to match amount of invocations in "then"
+
+        then:
+        7 * math.add(_,_) //"x *" tells program to call function x amount of times [invocations]
     }
 }
